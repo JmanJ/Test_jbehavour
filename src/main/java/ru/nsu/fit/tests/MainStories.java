a3bd34047a671175aac56c2a04a16c4c93560904
jmanj <jmanj@mail.ru>
2015-12-30 02:55:05 +0300

    3

--- src/main/java/ru/nsu/fit/tests/MainStories.java
+++ src/main/java/ru/nsu/fit/tests/MainStories.java
@@ -42,7 +42,7 @@ public class MainStories extends JUnitStories {
                         .withFailureTrace(true)
                         .withFailureTraceCompression(true));
     }
-//2
+//3
     @Override
     public InjectableStepsFactory stepsFactory() {
         return new InstanceStepsFactory(configuration(), new MainSteps(), new HousewifeSteps(), new SchoolboySteps(),
